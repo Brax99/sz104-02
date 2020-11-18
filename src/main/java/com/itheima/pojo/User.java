169bbd7d07e7d1b429ef773badf85b17a306a82f
mao <939722387@qq.com>
2020-11-18 17:14:56 +0800

    初始化

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,11 @@
+package com.itheima.pojo;
+
+/**
+ * @author mao
+ * @version 1.8
+ * @时间 2020/11/18  17:04
+ */
+public class User {
+    private  String username;
+    private  String password;
+}
